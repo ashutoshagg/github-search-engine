0acf6b323ca8cd30f292b229b05824967b2d5a79
Chehak Aggarwal <ashutosh.aggarwal4@gmail.com>
2018-09-10 12:24:01 +0530

    moved to angular 5 version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'github-search-engine';
+  title = 'My First Angular App!';
 }
